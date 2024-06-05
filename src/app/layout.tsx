82f62aa8beb3d785311c501695017ad2443165a7
Santlago <breno.santiago.8@gmail.com>
2024-06-05 09:19:40 -0300

    fix: icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  icons: "/logo.png",
+  icons: "/icon.png",
   title: "BlueHope",
   description: "Ajude a salvar os oceanos",
 };
